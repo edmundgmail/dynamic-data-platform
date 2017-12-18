14d0422ff3991078d35d9d74d5c65cd89aa2dd3a
Edmund Guo <edmund.guo@rbc.com>
2017-12-18 11:24:07 -0500

    initial draft

---
+++ src/app/views/components/data-explorer/data-explorer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-explorer',
+  templateUrl: './data-explorer.component.html',
+  styleUrls: ['./data-explorer.component.scss']
+})
+export class DataExplorerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
