adf14a8b35a6abdae729af4e6978a7c98823398e
Edmund Guo <edmund.guo@rbc.com>
2018-01-12 15:40:36 -0500

    update

--- src/app/views/components/sql.snippet.component.ts
+++ src/app/views/components/sql.snippet.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {WebSocketService} from '../../services/websocket.service';
 import {Subject} from 'rxjs/Subject';
 import {Message} from "../../models/message";
+import {TabsModule} from "ngx-tabs";
+
 
 const CHAT_URL = "ws://localhost:5000/ws";
 
@@ -12,6 +14,17 @@ const CHAT_URL = "ws://localhost:5000/ws";
 export class SQLSnippetComponent {
 
   messages: Subject<Message>  = new Subject<Message>();
+
+
+  public tabs: any[] = [
+    {title: 'Dynamic Title 1', content: 'dyna1'},
+    {title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true},
+    {title: 'Dynamic Title 3', content: 'Dynamic content 3', removable: true}
+  ];
+
+  public removeTabHandler(){
+
+  }
 
   constructor(private wsService: WebSocketService) {
     this.messages   = <Subject<Message>>this.wsService
